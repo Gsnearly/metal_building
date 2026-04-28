1b49d6935adb7d991183bd6ea845683d18cf8d81
Garrett B Snearly <gman100225@gmail.com>
2026-04-27 21:44:17 -0500

    Delete metal-building-configurator directory

--- metal-building-configurator/src/components/ControlGroup.jsx
+++
@@ -1,8 +0,0 @@
-export default function ControlGroup({ title, children }) {
-  return (
-    <div className="control-group">
-      <h3>{title}</h3>
-      <div className="control-list">{children}</div>
-    </div>
-  );
-}
